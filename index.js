2177c626d639e4a8e7598f41584b77f3b9601902
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-04-04 23:15:43 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router/routes.js");
+const path = require("path");
 
 const app = express();
 
@@ -9,10 +10,15 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/posts", router);
+app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
   console.log("GET REQ");
   res.send("GETTING REQUEST SUCCESSFULLY");
+});
+
+app.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname + "/static/index.html"));
 });
 
 app.listen(PORT, () => {
